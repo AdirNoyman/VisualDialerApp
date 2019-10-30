6f3fd4d1e03a43f3dfc5cd48d00f53703ad8b8a2
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2019-10-30 22:27:02 +0200

    Add files via upload

--- src/DogList.js
+++ src/DogList.js
@@ -11,10 +11,10 @@ class DogList extends Component {
                 <div className="row">
                     <div id="services" className="card col-lg-4">
                         <div className="card-title">שירות עצמי<br />Self Service</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1448932223592-d1fc686e76ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
-                                <p id="serviceP"><span style={{ color: "black" }}>◀︎</span>&nbsp;לחץ על הבחירה המבוקשת:</p>
+                                <p id="serviceP" style={{ fontWeight: "bold" }}><span style={{ color: "black" }}>◀︎</span>&nbsp;&nbsp;לחץ על הבחירה המבוקשת:</p>
                                 <ul>
                                     <li><a className="self-info" href="/" target="_blank" rel="noopener noreferrer">פתיחת קריאה</a></li>
                                     <li><a className="self-info" href="/" target="_blank" rel="noopener noreferrer">(FreePass)איפוס סיסמה בשירות עצמי</a></li>
@@ -25,10 +25,13 @@ class DogList extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="row">
+
+                <div className="row" id="main">
+                    <h2 className="divider gradient col-md-12" contentEditable>חיוג ישיר לתמיכה</h2>
+
                     <div className="card col-lg-4">
                         <div className="card-title" id="passwords">סיסמאות</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1509822929063-6b6cfc9b42f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -43,7 +46,7 @@ class DogList extends Component {
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="citrix">Citrix  סביבת</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -51,6 +54,7 @@ class DogList extends Component {
                                     <li>סריקה והדפסה</li>
                                     <li><a className="more-info" href={dummy} target="_blank" rel="noopener noreferrer">למידע נוסף - לחץ על הקישור</a></li>
                                 </ul>
+
                             </div>
                             <a href="tel:03-746-3535" className="dial btn btn-primary">חייג לתמיכה</a>
                         </div>
@@ -72,20 +76,22 @@ class DogList extends Component {
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="goodHead">"חברת "ראש טוב</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
 
                         <div className="card-body">
                             <div className="card-text">
+                                <br />
                                 <ul>
                                     <li><a className="more-info" href={dummy} target="_blank" rel="noopener noreferrer">למידע נוסף - לחץ על הקישור</a></li>
                                 </ul>
+                                <br />
                             </div>
                             <a href="tel:1-800-220-110" className="dial btn btn-primary">חייג לתמיכה</a>
                         </div>
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="doctors">רופאים עצמאיים</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1551190822-a9333d879b1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -100,7 +106,7 @@ class DogList extends Component {
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="docLine">קו לרופא</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -115,19 +121,21 @@ class DogList extends Component {
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="pay">זכאות</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
+                                <br />
                                 <ul>
                                     <li><a className="more-info" href={dummy} target="_blank" rel="noopener noreferrer">למידע נוסף - לחץ על הקישור</a></li>
                                 </ul>
+                                <br />
                             </div>
                             <a href="tel:03-514-3635" className="dial btn btn-primary">חייג לתמיכה</a>
                         </div>
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="teleRadio">טלרדיולוגיה</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1530497610245-94d3c16cda28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -142,7 +150,7 @@ class DogList extends Component {
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="sap">SAP</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -157,7 +165,7 @@ class DogList extends Component {
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="equipment">ציוד קצה</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1523120974498-9d764390d8e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -173,7 +181,7 @@ class DogList extends Component {
 
                     <div className="card col-lg-4">
                         <div className="card-title" id="telephone">תקשורת וטלפוניה</div>
-                        <img src="https://images.pexels.com/photos/1181336/pexels-photo-1181336.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1525598912003-663126343e1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -188,7 +196,7 @@ class DogList extends Component {
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="stations">עמדות שירות</div>
-                        <img src="https://images.pexels.com/photos/2058128/pexels-photo-2058128.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
@@ -203,7 +211,7 @@ class DogList extends Component {
                     </div>
                     <div className="card col-lg-4">
                         <div className="card-title" id="suppliers">ספקים</div>
-                        <img src="https://images.pexels.com/photos/42273/doctor-medical-medicine-health-42273.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="card-img-top" alt="..." />
+                        <img src="https://images.unsplash.com/photo-1516216628859-9bccecab13ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <div className="card-text">
                                 <ul>
